7cef101e767d6b726b270b569f79fc59ee751df5
ffaass <rlatnghd95@gmail.com>
2020-04-15 21:50:43 +0900

    initial commit

---
+++ src/main/java/kr/ac/korea/kkumtree/KkumtreeApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.korea.kkumtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkumtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkumtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/korea/kkumtree/KkumtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.korea.kkumtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkumtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
